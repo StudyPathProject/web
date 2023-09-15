8e179002be01f6ab699372c3c8cea47260e5d85c
MateoDi9z <dzmateozd@gmail.com>
2023-09-14 21:18:35 -0300

    fixed navbar responsive

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,42 +1,76 @@
 <template>
   <Disclosure as="nav" class="bg-main" v-slot="{ open }">
+    <!-- Mobile -->
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
           <DisclosureButton
-            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-main hover:bg-main-2 hover:text-main-2 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          >
             <span class="absolute -inset-0.5" />
             <span class="sr-only">Open main menu</span>
             <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
             <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
           </DisclosureButton>
-        </div>
 
-        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <div class="flex flex-shrink-0 items-center">
+          <div class="flex md:hidden flex-shrink-0 items-center">
             <CommonBrand :logo="true" />
           </div>
+        </div>
+
+        <!-- Left -->
+        <div
+          class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
+        >
+          <div class="flex-shrink-0 items-center sm:flex hidden">
+            <CommonBrand :logo="true" />
+          </div>
+
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
-              <NuxtLink v-for="item in navigation" :key="item.name" :to="item.href" class="transition-all"
-                :class="[item.current ? 'bg-main text-white' : 'text-main hover:bg-[#2e8a7c67] hover:text-[#2d4261]', 'rounded-md px-3 py-2 text-sm font-medium']"
-                :aria-current="item.current ? 'page' : undefined">{{ item.name }}</NuxtLink>
+              <NuxtLink
+                v-for="item in navigation"
+                :key="item.name"
+                :to="item.href"
+                class="transition-all"
+                :class="[
+                  item.current
+                    ? 'bg-main text-white'
+                    : 'text-main hover:bg-[#2e8a7c67] hover:text-[#2d4261]',
+                  'rounded-md px-3 py-2 text-sm font-medium',
+                ]"
+                :aria-current="item.current ? 'page' : undefined"
+                >{{ item.name }}</NuxtLink
+              >
             </div>
           </div>
         </div>
 
-        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <CommonButton variant="main-2">Iniciar Sesión</CommonButton>
+        <!-- Right -->
+        <div
+          class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+        >
+          <CommonButton variant="main-2" class="sm:flex hidden"
+            >Iniciar Sesión</CommonButton
+          >
 
-          <div class="h-8 w-[1px] mx-3 rounded-full bg-black"></div>
+          <div
+            class="h-8 w-[1px] mx-3 rounded-full bg-[#333] sm:block hidden"
+          ></div>
 
           <div class="flex" v-for="red in redes" :key="red.svg">
             <a :href="red.href" target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" :fill="iconColor" x="0px" y="0px" width="40" height="40"
-                viewBox="0 0 24 24">
-                <path :d="red.svg">
-                </path>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                :fill="iconColor"
+                x="0px"
+                y="0px"
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+              >
+                <path :d="red.svg"></path>
               </svg>
             </a>
           </div>
@@ -85,39 +119,61 @@
       </div>
     </div>
 
+    <!-- Hidden until mobile -->
     <DisclosurePanel class="sm:hidden">
       <div class="space-y-1 px-2 pb-3 pt-2">
-        <DisclosureButton v-for=" item  in  navigation " :key="item.name" as="a" :href="item.href"
-          :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
-          :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+        <DisclosureButton
+          v-for="item in navigation"
+          :key="item.name"
+          as="a"
+          :href="item.href"
+          :class="[
+            item.current
+              ? 'bg-gray-900 text-white'
+              : 'text-gray-200 hover:bg-gray-700 hover:text-white',
+            'block rounded-md px-3 py-2 pb-4 text-base font-semibold',
+          ]"
+          :aria-current="item.current ? 'page' : undefined"
+          >{{ item.name }}</DisclosureButton
+        >
+
+        <CommonButton variant="main-2">Iniciar Sesión</CommonButton>
       </div>
     </DisclosurePanel>
   </Disclosure>
 </template>
 
 <script setup>
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/vue";
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
 
-const iconColor = "#333"
+const iconColor = "#333";
 
 const redes = [
   {
     svg: "M 8 3 C 5.239 3 3 5.239 3 8 L 3 16 C 3 18.761 5.239 21 8 21 L 16 21 C 18.761 21 21 18.761 21 16 L 21 8 C 21 5.239 18.761 3 16 3 L 8 3 z M 18 5 C 18.552 5 19 5.448 19 6 C 19 6.552 18.552 7 18 7 C 17.448 7 17 6.552 17 6 C 17 5.448 17.448 5 18 5 z M 12 7 C 14.761 7 17 9.239 17 12 C 17 14.761 14.761 17 12 17 C 9.239 17 7 14.761 7 12 C 7 9.239 9.239 7 12 7 z M 12 9 A 3 3 0 0 0 9 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 12 A 3 3 0 0 0 12 9 z",
-    href: "https://www.instagram.com/studypathonline/"
+    href: "https://www.instagram.com/studypathonline/",
   },
   {
-    svg: "M 6 3 C 4.3550302 3 3 4.3550302 3 6 L 3 18 C 3 19.64497 4.3550302 21 6 21 L 18 21 C 19.64497 21 21 19.64497 21 18 L 21 6 C 21 4.3550302 19.64497 3 18 3 L 6 3 z M 12 7 L 14 7 C 14 8.005 15.471 9 16 9 L 16 11 C 15.395 11 14.668 10.734156 14 10.285156 L 14 14 C 14 15.654 12.654 17 11 17 C 9.346 17 8 15.654 8 14 C 8 12.346 9.346 11 11 11 L 11 13 C 10.448 13 10 13.449 10 14 C 10 14.551 10.448 15 11 15 C 11.552 15 12 14.551 12 14 L 12 7 z"
+    svg: "M 6 3 C 4.3550302 3 3 4.3550302 3 6 L 3 18 C 3 19.64497 4.3550302 21 6 21 L 18 21 C 19.64497 21 21 19.64497 21 18 L 21 6 C 21 4.3550302 19.64497 3 18 3 L 6 3 z M 12 7 L 14 7 C 14 8.005 15.471 9 16 9 L 16 11 C 15.395 11 14.668 10.734156 14 10.285156 L 14 14 C 14 15.654 12.654 17 11 17 C 9.346 17 8 15.654 8 14 C 8 12.346 9.346 11 11 11 L 11 13 C 10.448 13 10 13.449 10 14 C 10 14.551 10.448 15 11 15 C 11.552 15 12 14.551 12 14 L 12 7 z",
   },
   {
-    svg: "M21.582,6.186c-0.23-0.86-0.908-1.538-1.768-1.768C18.254,4,12,4,12,4S5.746,4,4.186,4.418 c-0.86,0.23-1.538,0.908-1.768,1.768C2,7.746,2,12,2,12s0,4.254,0.418,5.814c0.23,0.86,0.908,1.538,1.768,1.768 C5.746,20,12,20,12,20s6.254,0,7.814-0.418c0.861-0.23,1.538-0.908,1.768-1.768C22,16.254,22,12,22,12S22,7.746,21.582,6.186z M10,15.464V8.536L16,12L10,15.464z"
-  }
-]
+    svg: "M21.582,6.186c-0.23-0.86-0.908-1.538-1.768-1.768C18.254,4,12,4,12,4S5.746,4,4.186,4.418 c-0.86,0.23-1.538,0.908-1.768,1.768C2,7.746,2,12,2,12s0,4.254,0.418,5.814c0.23,0.86,0.908,1.538,1.768,1.768 C5.746,20,12,20,12,20s6.254,0,7.814-0.418c0.861-0.23,1.538-0.908,1.768-1.768C22,16.254,22,12,22,12S22,7.746,21.582,6.186z M10,15.464V8.536L16,12L10,15.464z",
+  },
+];
 
 const navigation = [
-  { name: 'Cursos', href: '/cursos', current: false },
+  { name: "Cursos", href: "/cursos", current: false },
   // { name: 'Team', href: '#', current: false },
   // { name: 'Projects', href: '#', current: false },
   // { name: 'Calendar', href: '#', current: false },
-]
+];
 </script>
