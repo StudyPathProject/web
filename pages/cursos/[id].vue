9c6a29045eca5358f838744c12fb204639498843
DHipo <104331457+DHipo@users.noreply.github.com>
2023-09-25 22:41:56 -0300

    fix display content

--- pages/cursos/[id].vue
+++ pages/cursos/[id].vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import {
-    Disclosure,
-    DisclosureButton
-} from '@headlessui/vue'
 import { ArrowLeftCircleIcon } from '@heroicons/vue/24/solid'
 import { getCapitulos, getContenidos } from '~/middlewares/dao';
 import { Capitulo, Contenido } from '~/types/cursos';
@@ -15,10 +11,7 @@ const curso: { Capitulos: Capitulo[], name: string } = await getCapitulos(id.toS
     .then((res) => {
         res.curso.data.attributes.Capitulos.map(async (e) => {
             contenidoDelCurso.push(await getContenidos(id.toString(), e.name)
-                .then((res) => {
-                    console.log(res.curso.data.attributes.Capitulos[0])
-                    return res.curso.data.attributes.Capitulos[0]
-                })
+                .then((res) => res.curso.data.attributes.Capitulos[0])
             )
         })
 
@@ -26,7 +19,6 @@ const curso: { Capitulos: Capitulo[], name: string } = await getCapitulos(id.toS
     })
     .catch((err) => { console.log(err); return err; })
 
-console.log(curso, contenidoDelCurso)
 let showOptions: Ref<boolean> = ref(true)
 let SelectedCap: Ref<Capitulo | null> = ref(null)
 
@@ -35,6 +27,14 @@ function changeSelectedOption(param: Capitulo) {
     // SelectedCap.value.Contenido.forEach((e) => console.log(e))
     // console.log(SelectedCap.value.Contenido.values())
 }
+if (process.client) {
+    document.addEventListener("keydown", (event: KeyboardEvent) => {
+        if (event.key === "Escape") {
+            SelectedCap.value = null    
+        }
+    });
+}
+
 </script>
 
 <template>
@@ -45,16 +45,15 @@ function changeSelectedOption(param: Capitulo) {
 
             <h2 class="text-3xl min-h-16 w-full font-semibold text-black mb-4">{{ curso.name }} </h2>
 
-            <Disclosure as="div" v-for="capitulo in contenidoDelCurso" class="md:text-xl text-2xl md:mr-5"
-                v-slot="{ open }">
-                <DisclosureButton class="flex flex-col w-full justify-between text-left font-medium p-2"
+            <div v-for="capitulo in contenidoDelCurso" class="md:text-xl text-2xl md:mr-5">
+                <div class="flex flex-col w-full justify-between text-left font-medium p-2"
                     v-on:click="changeSelectedOption(capitulo)">
                     <span>{{ capitulo.name }}</span>
                     <div class="transition-opacity duration-700 ease-in"
-                        :class="open ? 'h-1 w-10 bg-main-2-darker shadow-xl rounded-xl mr-5 opacity-100' : 'opacity-0'">
+                        :class="capitulo.name == SelectedCap?.name ? 'h-1 w-10 bg-main-2-darker shadow-xl rounded-xl mr-5 opacity-100' : 'opacity-0'">
                     </div>
-                </DisclosureButton>
-            </Disclosure>
+                </div>
+            </div>
         </div>
 
         <div v-if="SelectedCap != null" class="text-left md:w-full text-black w-screen md:mx-10 -mx-5">
@@ -63,13 +62,14 @@ function changeSelectedOption(param: Capitulo) {
                 <h1 class="font-bold text-4xl">{{ SelectedCap.name }}</h1>
             </div>
 
-            <div v-for="contenido in SelectedCap['Contenido']" class="flex flex-col w-full mt-10 mb-10 justify-center items-center gap-10">
+            <div v-for="contenido in SelectedCap['Contenido']"
+                class="flex flex-col w-full mt-10 mb-10 justify-center items-center gap-10">
 
-                <Markdown v-if="contenido.text"
-                    class="font-normal leading-relaxed text-xl md:w-full text-left mx-5"
+                <Markdown v-if="contenido.text" class="font-normal leading-relaxed text-xl md:w-full text-left mx-5"
                     :markdownString="contenido.text" />
 
-                <img v-if="contenido.image" v-for="img in contenido.image.data" class="h-full aspect-auto md:w-5/6 w-full rounded-xl shadow-xl"
+                <img v-if="contenido.image" v-for="img in contenido.image.data"
+                    class="h-full aspect-auto md:w-5/6 w-full rounded-xl shadow-xl"
                     :src="`https://strappi-production.up.railway.app${img.attributes.formats.medium.url}`" />
 
             </div>
