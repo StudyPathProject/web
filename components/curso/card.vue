dc10829851799ff849c72b20a465d579fcfbed9f
DHipo <104331457+DHipo@users.noreply.github.com>
2023-10-09 08:49:58 -0300

    fix merge crad

--- components/curso/card.vue
+++ components/curso/card.vue
@@ -25,9 +25,8 @@ export default {
 <template>
   <div class="card flex-1 min-w-[250px] h-fit bg-gray-200 px-0 mix-h-42 shadow-x mt-10 max-w-[380px]">
     <figure class="max-h-[150px] object-cover w-full px-0">
-      <img :src="curso.attributes?.banner ?? '/_nuxt/assets/img/curso-ejemplo.png'" class="h-full w-full" alt="Curso" />
+      <img :src="curso.attributes?.banner ? '/_nuxt/assets/img/curso-ejemplo.png'" class="h-full w-full" />
     </figure>
-
     <div class="card-body">
       <h2 class="card-title">{{ curso.attributes.name }}</h2>
       <p>{{ curso.attributes.description }}</p>
