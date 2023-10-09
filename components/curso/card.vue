83332645243d868bede8c9eb4e89e7c05cfa07b9
DHipo <104331457+DHipo@users.noreply.github.com>
2023-10-09 09:04:02 -0300

    fix card

--- components/curso/card.vue
+++ components/curso/card.vue
@@ -25,7 +25,7 @@ export default {
 <template>
   <div class="card flex-1 min-w-[250px] h-fit bg-gray-200 px-0 mix-h-42 shadow-x mt-10 max-w-[380px]">
     <figure class="max-h-[150px] object-cover w-full px-0">
-      <img :src="curso.attributes?.banner ? '/_nuxt/assets/img/curso-ejemplo.png'" class="h-full w-full" />
+      <img :src="curso.attributes?.banner ? '/_nuxt/assets/img/curso-ejemplo.png' : ''" class="h-full w-full" />
     </figure>
     <div class="card-body">
       <h2 class="card-title">{{ curso.attributes.name }}</h2>
