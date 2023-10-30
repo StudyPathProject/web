df14ff1cf2604b38c3c58a64d569375fd199f169
Mateo <dzmateozd@gmail.com>
2023-10-30 18:50:55 -0300

    fix

--- stores/auth.ts
+++ stores/auth.ts
@@ -27,7 +27,7 @@ export const useAuthStore = defineStore("auth", {
     async TryLogin(payload: { email: string; password: string }) {
       const { email, password } = payload;
       const env = useRuntimeConfig();
-      console.log(env.app)
+
       try {
         const url = `${env.app.API_BASE!}/api/auth/local?populate=role`;
         await $fetch(url, {
